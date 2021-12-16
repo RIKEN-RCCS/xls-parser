a56796a759208ed43279a890b6276c7443b928d5
Emil VATAI <emil.vatai@gmail.com>
2021-12-16 16:42:57 +0900

    WIP `linreg.py`

--- linreg.py
+++ linreg.py
@@ -1,16 +1,40 @@
 #!/usr/bin/env python
+import argparse
+
 import pandas as pd
 
-derived = pd.read_csv("all.tsv", sep="\t")
-print(derived.shape)
 
-diffs = pd.read_csv("polybench.tsv", sep="\t")
-diffs = diffs.iloc[:, [0, 2, 3, 4]]
-print(diffs.shape)
+def get_cleaned(derived_path: str, diffs_path: str) -> pd.DataFrame:
+    selected_diffs_cols = [0, 2, 3, 4]
+    last_removed_col = 8
 
-merged = pd.merge(derived, diffs, on="Benchmark")
-print(merged.shape)
-print(merged.head())
+    derived = pd.read_csv(derived_path, sep="\t")
+    diffs = pd.read_csv(diffs_path, sep="\t")
+    diffs = diffs.iloc[:, selected_diffs_cols]
+    merged = pd.merge(derived, diffs, on="Benchmark")
+    removed_cols = merged.iloc[:, :last_removed_col].columns
+    print(f"Removed cols: {[c for c in removed_cols]}")
+    cleaned = merged.iloc[:, last_removed_col:]
+    return cleaned
 
-print(set(derived["Benchmark"]) - set(diffs["Benchmark"]))
-print(set(diffs["Benchmark"]) - set(derived["Benchmark"]))
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--derived_path",
+        help="TSV from xml/xls flatten script",
+        default="all.tsv",
+    )
+    parser.add_argument(
+        "--diffs_path",
+        help="TSV from google sheets",
+        default="polybench.tsv",
+    )
+    args = parser.parse_args()
+    data = get_cleaned(args.derived_path, args.diffs_path)
+    print(data.head())
+    # for c in merged.columns:
+    #     print(merged[c].dtype, c)
+
+
+main()
